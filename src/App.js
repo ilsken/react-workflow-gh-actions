37ec1b40e39e34f74d8bf1b5a036b67267a8635d
Christopher Tarquini <code@tarq.io>
2021-05-20 15:39:14 -0400

    add launchdarkly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withLDConsumer } from 'launchdarkly-react-client-sdk';
+
+const ShinyNewFeature =  withLDConsumer(({flags}) => {
+  if (flags.enableShinyNewFeature) {
+    return (
+      <p>Hello, the shiny new feature is on</p>
+    )
+  }
+})
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
+      <p>
+        <ShinyNewFeature />
+      </p>
     </div>
   );
 }
 
-export default App;
+export default withLDProvider({
+  clientSideID: process.env.LD_CLIENT_ID,
+  user: {
+    anonymous: true,
+    custom: {
+      'Build Version': BUILD_VERSION,
+      'Build Name': BUILD_NAME
+    }
+  },
+  options: { /* ... */ }
+})(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { withLDProvider } from 'launchdarkly-react-client-sdk';
+const {version: BUILD_VERSION, name: BUILD_NAME} = require('../package.json')
+
+const AppWithLD = withLDProvider({
+  clientSideID: process.env.LD_CLIENT_ID,
+  user: {
+    anonymous: true,
+    custom: {
+      'Build Version': BUILD_VERSION,
+      'Build Name': BUILD_NAME
+    }
+  },
+  options: { /* ... */ }
+})(App);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppWithLD />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
